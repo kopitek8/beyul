1ff7ff65dc6593d844209aa4fa505e6b586f3036
Keyur Khadka <25280806+kopitek8@users.noreply.github.com>
2022-09-25 12:30:16 +0545

    Add skeleton pages for every components

---
+++ components/Expect/index.tsx
@@ -0,0 +1,14 @@
+const Expect = () => {
+  return (
+    <div>
+      <p>
+        What to Expect: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Distinctio consequatur ullam libero eveniet molestias esse quae
+        assumenda, laboriosam temporibus, quisquam ipsa laudantium quod mollitia
+        dolorum unde pariatur neque illo ex.
+      </p>
+    </div>
+  );
+};
+
+export default Expect;
---
+++ components/Journey/index.tsx
@@ -0,0 +1,14 @@
+const Journey = () => {
+  return (
+    <div>
+      <p>
+        The Journey: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Distinctio consequatur ullam libero eveniet molestias esse quae
+        assumenda, laboriosam temporibus, quisquam ipsa laudantium quod mollitia
+        dolorum unde pariatur neque illo ex.
+      </p>
+    </div>
+  );
+};
+
+export default Journey;
