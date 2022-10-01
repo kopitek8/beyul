47b39437df5327199477b701ba84100e25f69255
Keyur Khadka <25280806+kopitek8@users.noreply.github.com>
2022-10-01 15:44:22 +0545

    Add parallax wrapper and comment layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    // <Layout>
+    <ParallaxProvider>
       <Component {...pageProps} />
-    </Layout>
+    </ParallaxProvider>
+    // </Layout>
   );
 }
 
