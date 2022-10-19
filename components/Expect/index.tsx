2a74988f3d84a043f6d9e4b966bae2915abfcbb2
Keyur Khadka <25280806+kopitek8@users.noreply.github.com>
2022-10-19 22:04:56 +0545

    Set and render initial design

--- components/Expect/index.tsx
+++ components/Expect/index.tsx
@@ -1,14 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from '../../styles/Expect.module.css';
+import Image from 'next/image';
+
+import bonfire from '../../public/assets/Expect/bonfire.png';
+import camping from '../../public/assets/Expect/camping-trek.png';
+import dining from '../../public/assets/Expect/dining.png';
+import guide from '../../public/assets/Expect/guide.png';
+
+const expectArr = [
+  {
+    src: bonfire,
+    alt: 'Campfire Every Night',
+  },
+  {
+    src: camping,
+    alt: 'Outdoor Camping',
+  },
+  {
+    src: dining,
+    alt: 'Five Billion Star Winning and Dining',
+  },
+  {
+    src: guide,
+    alt: 'Classy Guide and Security',
+  },
+  {
+    src: bonfire,
+    alt: 'Campfire Every Night',
+  },
+  {
+    src: camping,
+    alt: 'Outdoor Camping',
+  },
+  {
+    src: dining,
+    alt: 'Five Billion Star Winning and Dining',
+  },
+];
+
+const renderItem = (key, src, alt) => {
+  return (
+    <div>
+      <Image
+        key={key}
+        src={src}
+        alt={alt}
+        // layout='fill'
+        objectFit='contain'
+        height={300}
+        width={300}
+      ></Image>
+      <p>{alt}</p>
+    </div>
+  );
+};
 
 const Expect: React.FC = () => {
   return (
-    <div>
-      <p>
-        What to Expect: Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Distinctio consequatur ullam libero eveniet molestias esse quae
-        assumenda, laboriosam temporibus, quisquam ipsa laudantium quod mollitia
-        dolorum unde pariatur neque illo ex.
-      </p>
+    <div className={styles.container}>
+      <h1 className={styles.header}>what to expect</h1>
+      <div className={styles.imageContainer}>
+        {expectArr.map((arr, key) => renderItem(key, arr.src, arr.alt))}
+      </div>
     </div>
   );
 };
