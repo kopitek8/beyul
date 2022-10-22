1efbd2579d3285cf9c33edb4b9c6638d60ee4ad3
Keyur Khadka <25280806+kopitek8@users.noreply.github.com>
2022-10-22 14:47:49 +0545

    Add type to util file

--- utilities/useMousePosition.tsx
+++ utilities/useMousePosition.tsx
@@ -2,11 +2,17 @@
 
 import { useEffect, useState } from 'react';
 
+interface MouseInterface {
+  clientX: number;
+  clientY: number;
+}
+
 export const useMousePosition = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    const setFromEvent = (e) => setPosition({ x: e.clientX, y: e.clientY });
+    const setFromEvent = (e: MouseInterface) =>
+      setPosition({ x: e.clientX, y: e.clientY });
     window.addEventListener('mousemove', setFromEvent);
 
     return () => {
