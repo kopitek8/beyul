5fd205c6c73d365cb64c9ad10735e009052ccd6b
Keyur Khadka <25280806+kopitek8@users.noreply.github.com>
2022-10-22 12:09:47 +0545

    Add join us component

--- components/JoinUs/index.tsx
+++ components/JoinUs/index.tsx
@@ -1,14 +1,43 @@
 import React from 'react';
+import styles from '../../styles/JoinUs.module.css';
+import Image from 'next/image';
+
+import skill from '../../public/assets/JoinUs/skill.png';
+
+const expectArr = [
+  {
+    src: skill,
+    alt: 'Nominate Your Village',
+  },
+  {
+    src: skill,
+    alt: 'Skill Share',
+  },
+];
+
+const renderItem = (key, src, alt) => {
+  return (
+    <div className={styles.item}>
+      <Image
+        key={key}
+        src={src}
+        alt={alt}
+        objectFit='contain'
+        height={300}
+        width={300}
+      />
+      <p className={styles.description}>{alt}</p>
+    </div>
+  );
+};
 
 const JoinUs: React.FC = () => {
   return (
-    <div>
-      <p>
-        Join Us: Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Distinctio consequatur ullam libero eveniet molestias esse quae
-        assumenda, laboriosam temporibus, quisquam ipsa laudantium quod mollitia
-        dolorum unde pariatur neque illo ex.
-      </p>
+    <div className={styles.container}>
+      <h1 className={styles.header}>be a part of beyül</h1>
+      <div className={styles.imageContainer}>
+        {expectArr.map((arr, key) => renderItem(key, arr.src, arr.alt))}
+      </div>
     </div>
   );
 };
