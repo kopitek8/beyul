b6e2070cbd51c9576415fce1d50b81ab88e3b928
Keyur Khadka <25280806+kopitek8@users.noreply.github.com>
2022-10-22 12:37:12 +0545

    Comment out parallax effect

--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { Parallax } from 'react-scroll-parallax';
 import styles from '../../styles/Landing.module.css';
 
+// const dimension = Dimension.get(window);
+// console.log('di ', dimension);
+
 const Landing: React.FC = () => {
   return (
     <div className={styles.banner}>
-      <Parallax speed={30}>
+      {/* <Parallax speed={30}>
         <div className={styles.cloud}></div>
       </Parallax>
       <Parallax speed={80}>
         <div className={styles.person}></div>
-      </Parallax>
+      </Parallax> */}
     </div>
   );
 };
