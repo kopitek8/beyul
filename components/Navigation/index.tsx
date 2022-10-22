b5f3aadf2860780aee6bc9385a16e532c404e817
Keyur Khadka <25280806+kopitek8@users.noreply.github.com>
2022-10-22 15:57:51 +0545

    Add hamburger menu

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { IoIosMenu } from 'react-icons/io';
+
 import styles from '../../styles/Navigation.module.css';
 
 const Navigation: React.FC = () => {
   return (
     <div className={styles.container}>
-      <h1>Navigation</h1>
+      <IoIosMenu size={48} color={'white'} />
     </div>
   );
 };
