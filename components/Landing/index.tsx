1140979c644ea792d13d05469027adf2ff1ca12e
Keyur Khadka <25280806+kopitek8@users.noreply.github.com>
2022-10-31 21:53:27 +0545

    Add custom parallax

--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -14,65 +14,51 @@ import bottom from '../../public/assets/Parallax/bottom.png';
 import post from '../../public/assets/Parallax/post.png';
 
 import useDeviceSize from '../../utilities/useDeviceSize';
+import useCustomParallax from '../../utilities/useCustomParallax';
 
 const Landing: React.FC = () => {
   const [width, height] = useDeviceSize();
-
-  // const renderParallaxView = () => {
-  //   return (
-  //     <Parallax pages={5}>
-  //       <ParallaxLayer speed={1}>
-  //         <div className={styles.sky}>
-  //           <Image src={top} alt={'Sky and stars'} />
-  //         </div>
-  //       </ParallaxLayer>
-  //       <ParallaxLayer speed={1}>
-  //         <div className={styles.village}>
-  //           <Image src={middle} alt={'Village'} />
-  //         </div>
-  //       </ParallaxLayer>
-  //       <ParallaxLayer speed={3}>
-  //         <div className={styles.biguCloud}>
-  //           <Image src={logo} alt={'Bugy Life Festival'} />
-  //         </div>
-  //       </ParallaxLayer>
-  //       <ParallaxLayer speed={1}>
-  //         <div className={styles.yetiPost}>
-  //           <Image src={post} alt={'Beware of yeti'} />
-  //         </div>
-  //       </ParallaxLayer>
-  //       <ParallaxLayer speed={1}>
-  //         <div className={styles.stepstones}>
-  //           <Image src={bottom} alt={'Steps and stone'} />
-  //         </div>
-  //       </ParallaxLayer>
-  //     </Parallax>
-  //   );
-  // };
+  const [offsetY] = useCustomParallax();
 
   const renderMaxView = () => {
     return (
-      <>
-        <div className={styles.sky}>
+      <div>
+        <div
+        // style={{ transform: `translateY(${offsetY * 0.4}px)` }}
+        >
           <Image src={top} alt={'Sky and stars'} />
         </div>
-        <div className={styles.village}>
+        <div
+          className={styles.village}
+          style={{ transform: `translateY(-${offsetY * 0.15}px)` }}
+        >
           <Image src={middle} alt={'Village'} />
         </div>
-        <div className={styles.biguCloud}>
+        <div
+          className={styles.biguCloud}
+          style={{ transform: `translateY(-${offsetY * 0.6}px)` }}
+        >
           <Image src={logo} alt={'Bugy Life Festival'} />
         </div>
-        <div className={styles.yetiPost}>
+        <div
+          className={styles.yetiPost}
+          style={{ transform: `translateY(-${offsetY * 0.2}px)` }}
+        >
           <Image src={post} alt={'Beware of yeti'} />
         </div>
-        <div className={styles.stepstones}>
+        <div
+          className={styles.stepstones}
+          style={{ transform: `translateY(-${offsetY * 0.31}px)` }}
+        >
           <Image src={bottom} alt={'Steps and stone'} />
         </div>
-      </>
+      </div>
     );
   };
 
-  return <div className={styles.wrapper}>{width > 980 && renderMaxView()}</div>;
+  return (
+    <div className={styles.wrapper}>{width >= 764 && renderMaxView()}</div>
+  );
 };
 
 export default Landing;
