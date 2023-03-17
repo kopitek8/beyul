a448c1c3166b79a33db4bbf5d87e21615304d57c
Keyur Khadka <25280806+kopitek8@users.noreply.github.com>
2023-03-17 14:22:34 +0545

    Add send email feature

--- components/SendToLab/index.tsx
+++ components/SendToLab/index.tsx
@@ -1,18 +1,80 @@
-import React from 'react';
-import Link from 'next/link';
+import React, { useState } from 'react';
 import styles from '../../styles/SendToLab.module.css';
+import emailjs from '@emailjs/browser';
+import { toast } from "react-toastify";
 
-//Explore this : https://www.linkedin.com/pulse/how-send-email-next-js-app-module-zeel-prajapati/
+interface SendEmail {
+  name: string,
+  email: string,
+}
 
-const SendToLab: React.FC = () => {
+const SendToLab = (props: SendEmail) => {
+
+  const [isClicked, setClicked] = useState(false);
+  const [values, setValues] = useState({ name: '', email: '' })
+
+  const sendMail = async (e) => {
+    e.preventDefault();
+    emailjs.send(
+      process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID,
+      process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID,
+      values,
+      process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY)
+      .then(function (response) {
+        console.log('SUCCESS!', response.status, response.text);
+        toast('We appreciate your interest. We will get back to you shortly.',
+          {
+            hideProgressBar: true,
+            autoClose: 5000,
+            type: 'success'
+          })
+      }, function (error) {
+        toast('We could not receive your interest at the moment. Please try again.',
+          {
+            hideProgressBar: true,
+            autoClose: 5000,
+            type: 'error'
+          })
+      });
+  }
+
+
   return (
     <div className={styles.container}>
-      <Link href="/">
-        <button onClick={() => console.log('Clickd')} className={styles.button}>
-          <p className={styles.content}>I&apos;m Interested</p>
-        </button>
-      </Link>
-    </div>
+      <button
+        onClick={() => {
+          console.log('Clicekd');
+          setClicked(!isClicked);
+        }}
+        className={styles.button}>
+        <p className={styles.content}>I&apos;m Interested</p>
+      </button>
+      {isClicked &&
+        <div className={styles.modal}>
+          <form>
+            <input
+              type="text"
+              placeholder="What's your name?"
+              required
+              value={values.name}
+              onChange={
+                (e) => setValues({ ...values, name: e.target.value })
+              }
+              className={styles.input}
+            />
+            <input
+              type="email"
+              placeholder="What's your email?"
+              required
+              value={values.email}
+              onChange={
+                (e) => setValues({ ...values, email: e.target.value })}
+            />
+          </form>
+          <button onClick={sendMail}>Send to Lab</button>
+        </div>
+      }
+    </div >
   );
 };
 
